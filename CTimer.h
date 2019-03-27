1b689d2568a4dfe2c956d8550ee2253b7b50532a
fxconfig <peter1946073945@gmail.com>
2019-03-28 00:22:41 +0800

    Update CTimer.h

--- CTimer.h
+++ CTimer.h
@@ -1,129 +1,133 @@
-/*  ”√∑®”Î”√¡ø
+/*  Áî®Ê≥ï‰∏éÁî®Èáè
 
 static void TimerHandler1(void* p)
 {
-    printf("TimerHandler11111  x = %d\n",*(int*)p );
+	printf("TimerHandler11111  x = %d\n",*(int*)p );
 }
 static void TimerHander2(void* p)
 {
-    printf("Timer22\n");
+	printf("Timer22\n");
 }
 
 	TimerManager tm( 100 );
 	tm.StartTimerManager();
-    Timer t(tm);
+	Timer t(tm);
 	Timer t2(tm);
 
 	int x = 0;
-    t.Start(TimerHandler1, 1000, &x);
+	t.Start(TimerHandler1, 1000, &x);
 	t2.Start(TimerHander2,1333);
 
-	//Õ£÷π
+	//ÂÅúÊ≠¢
 	tm.StopTimerManager();
-*/
+*/ 
 
-#pragma once 
+#ifndef _PETER_C_TIMER_
+#define _PETER_C_TIMER_
 
-#ifdef _MSC_VER 
-# include <sys/timeb.h>
-#include <process.h>
-#include <Windows.h>
-
-#else
-#include <sys/time.h>
-#include <pthread.h>  
-#endif
 
 #include <vector>
-
-//∂® ±∆˜ªÿµ˜∫Ø ˝∂®“Â
-typedef   void(* TimerFunction)(void* p) ; 
-
-
-class TimerManager;
- 
-class Timer
-{
-public:
-    enum TimerType { ONCE, CIRCLE };
- 
-    Timer(TimerManager& manager);
-    ~Timer();
- 
-    //template<typename Fun>
-	void Start(TimerFunction fun, unsigned interval,  void* parm = NULL,TimerType timeType = CIRCLE);
-    void Stop();
-
-private:
-    void OnTimer(unsigned long long now);
- 
-private:
-    friend class TimerManager;
-    TimerManager& manager_;
-    TimerType timerType_;
-    TimerFunction timerFun_;
-	void *        timerFunParm_;
-    unsigned interval_;
-    unsigned long long expires_;
- 
-    size_t heapIndex_;
-};
+#include <functional>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <iostream>
 
 
- 
+//ÂÆöÊó∂Âô®ÂõûË∞ÉÂáΩÊï∞ÂÆö‰πâ
+//typedef   void(*TimerFunction)(void* p);
+using _timerFun = std::function<void()>;
+
+class Timer;
+
 class TimerManager
 {
 public:
-	TimerManager(int miniseconds = 50);
+	TimerManager(int miniseconds = 200);
 	~TimerManager();
-    static unsigned long long GetCurrentMillisecs();
-	
+	static unsigned long long GetCurrentMillisecs();
+
 	void StopTimerManager();
 	bool StartTimerManager();
 
 	void run();
-	//œﬂ≥Ã∫Ø ˝
-#ifdef WIN32  
-    static unsigned __stdcall thread_func(void* arg);  
-#else  
-    static void* thread_func(void* arg);  
-#endif  
- 
+
 private:
 
-    friend class Timer;
-    void AddTimer(Timer* timer);
-    void RemoveTimer(Timer* timer);
-    
- 
-    void UpHeap(size_t index);
-    void DownHeap(size_t index);
-    void SwapHeap(size_t, size_t index2);
- 
+	friend class Timer;
+	void AddTimer(Timer* timer);
+	void RemoveTimer(Timer* timer);
+
+
+	void UpHeap(size_t index);
+	void DownHeap(size_t index);
+	void SwapHeap(size_t, size_t index2);
+	void DetectTimers();
+
 private:
-	void DetectTimers();	 
 	int							m_interval;
-	volatile unsigned long		m_stop_flag;
-	HANDLE						m_handle;
-    struct HeapEntry
-    {
-        unsigned long long time;
-        Timer* timer;
-    };
-    std::vector<HeapEntry>		heap_;
+	std::atomic<bool>			m_stop_flag;
+	std::thread					m_thread;
+	std::condition_variable     m_cv;
+	struct HeapEntry
+	{
+		unsigned long long time;
+		Timer* timer;
+	};
+	std::vector<HeapEntry>		heap_;
 };
- 
 
 
-//template<typename Fun>
-inline void Timer::Start(TimerFunction fun, unsigned interval, void* parm, TimerType timeType)
+
+class Timer
 {
-    Stop();
-    interval_ = interval;
-    timerFun_ = fun;
-    timerType_ = timeType;
-	timerFunParm_ = parm;
-    this->expires_ = this->interval_ + TimerManager::GetCurrentMillisecs();
-    manager_.AddTimer(this);
-}
- 
+public:
+	enum TimerType { ONCE, CIRCLE };
+
+	Timer(TimerManager& manager) : m_manager(manager),m_heapIndex(-1) {};
+	~Timer() { Stop(); };
+
+	template<class Fun>
+	void Start(Fun fun, unsigned interval, TimerType timeType = CIRCLE) {
+		Stop();
+		m_interval = interval;
+		m_fun = fun;
+		m_timerType = timeType;
+		this->m_expires = this->m_interval + TimerManager::GetCurrentMillisecs();
+		m_manager.AddTimer(this);
+	};
+	void Stop() {
+		if (m_heapIndex != -1){
+			m_manager.RemoveTimer(this);
+			m_heapIndex = -1;
+		}
+	};
+
+private:
+	void OnTimer(unsigned long long now) {
+		if (m_timerType == Timer::CIRCLE)
+		{
+			m_expires = m_interval + now;
+			m_manager.AddTimer(this);
+		}
+		else
+		{
+			m_heapIndex = -1;
+		}
+		m_fun();
+	};
+
+private:
+	friend class   TimerManager;
+	TimerManager&  m_manager;
+	TimerType      m_timerType;
+	_timerFun      m_fun;
+	unsigned int   m_interval;
+	unsigned long long m_expires;
+	size_t		   m_heapIndex;
+};
+
+
+
+#endif
